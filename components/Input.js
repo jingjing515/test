934deb61e924e87ae181f5f1127d606c252e06e5
Neda <nchangizi@bcit.ca>
2023-01-17 18:58:32 -0800

    adds input component with textinput and passes data back to App.js

---
+++ components/Input.js
@@ -0,0 +1,19 @@
+import { View, TextInput } from "react-native";
+import { useState } from "react";
+
+export default function Input({ sendChangedText }) {
+  const [text, setText] = useState();
+  function changeTextHandler(changedText) {
+    setText(changedText);
+    sendChangedText(changedText);
+  }
+  return (
+    <View>
+      <TextInput
+        value={text}
+        onChangeText={changeTextHandler}
+        style={{ backgroundColor: "#eee" }}
+      />
+    </View>
+  );
+}
