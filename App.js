832949f2a6ff41df05800018a33fa3a8aa177ba4
Neda <nchangizi@bcit.ca>
2023-01-17 18:39:12 -0800

    adds textinput in App.js

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
 
 export default function App() {
   const name = "my awesome app";
-
+  const [text, setText] = useState();
+  function changeTextHandler(changedText) {
+    setText(changedText);
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header appName={name} />
+      <TextInput
+        value={text}
+        onChangeText={changeTextHandler}
+        style={{ backgroundColor: "#eee" }}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
